f3fbee327ec66c74fd974528985530962b4cc5d8
mehmetemineker <mehmetemineker92@gmail.com>
2024-08-26 22:21:47 +0300

    game page edit

--- pages/[gameId].vue
+++ pages/[gameId].vue
@@ -1,27 +1,35 @@
 <template>
-  <div class="flex flex-wrap justify-center gap-8 mb-10">
-    <UChip size="md" position="bottom-right" :ui="{ base: '-mx-2 rounded-none ring-0', background: '' }" :show="false">
-      <UButton size="xl" label="Mehmet Emin" color="gray" />
-      <template #content>
-        <UBadge color="white" variant="solid">☕</UBadge>
-      </template>
-    </UChip>
-  </div>
+  <div class="flex flex-col gap-6">
+    <div class="flex flex-wrap justify-center gap-8">
+      <UChip size="md" position="bottom-right" :ui="{ base: '-mx-2 rounded-none ring-0', background: '' }"
+        :show="pokerValue !== undefined">
+        <UButton size="xl" label="Mehmet Emin" color="gray" />
+        <template #content>
+          <!-- <UBadge color="blue" variant="solid">...</UBadge> -->
+          <UBadge color="white" variant="solid">{{ pokerValue }}</UBadge>
+        </template>
+      </UChip>
+    </div>
 
-  <UTabs :ui="{ list: { tab: { height: 'h-20' }, height: '' } }" :items="items" :default-index="defaultIndex"
-    :content="false" @change="onChange" />
+    <UTabs :unmount="true" :ui="{ list: { tab: { height: 'h-20' }, height: '' } }" :items="items"
+      :default-index="defaultIndex" :content="false" @change="onChange" />
+
+    <div class="flex justify-center">
+      <UButton>I'm okay</UButton>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const { $pokerTypes } = useNuxtApp()
 const pokerType = ref($pokerTypes.filter(m => m.name === "fibonacci")[0])
-const defaultIndex = ref(pokerType.value.defaultIndex)
+const defaultIndex = ref(-1)
 
 const items = computed(() => {
   return (pokerType.value.items as string[]).map(item => ({ label: item }))
 })
 
-const pokerValue = ref(items.value[defaultIndex.value].label)
+const pokerValue = ref(items.value[defaultIndex.value]?.label)
 
 const onChange = (index: number) => {
   pokerValue.value = items.value[index].label
--- plugins/globalVariable.ts
+++ plugins/globalVariable.ts
@@ -6,25 +6,21 @@ export default defineNuxtPlugin(() => {
           "name": "fibonacci",
           "label": "Fibonacci ( 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ?, ☕ )",
           "items": ["0", "1", "2", "3", "5", "8", "13", "21", "34", "55", "89", "?", "☕"],
-          "defaultIndex": 11
         },
         {
           "name": "modified-fibonacci",
           "label": "Modified Fibonacci ( 0, ½, 1, 2, 3, 5, 8, 13, 20, 40, 100, ?, ☕ )",
           "items": ["0", "½", "1", "2", "3", "5", "8", "13", "20", "40", "100", "?", "☕"],
-          "defaultIndex": 11
         },
         {
           "name": "t-shirt",
           "label": "T-shirts (XS, S, M, L, XL, ?, ☕ )",
           "items": ["XS", "S", "M", "L", "XL", "?", "☕"],
-          "defaultIndex": 5
         },
         {
           "name": "power-of-2",
           "label": "Powers of 2 ( 0, 1, 2, 4, 8, 16, 32, 64, ?, ☕ )",
           "items": ["0", "1", "2", "4", "8", "16", "32", "64", "?", "☕"],
-          "defaultIndex": 8
         }
       ]
     }
